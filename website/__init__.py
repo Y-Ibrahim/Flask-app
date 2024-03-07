a1015b5c2348f10a89601a7408f2f5674de0885d
Y-Ibrahim <yibro.i2001@gmail.com>
2024-03-07 17:50:37 +0000

    added create_app function

--- website/__init__.py
+++ website/__init__.py
@@ -1,17 +1,27 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy, session
 from os import path
-from flask_login import LoginManager
 
-def create_app():
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+
+def create_app(): 
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "helloworld"
 
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Blender_12@localhost/flask_aws'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     from .views import views
-    from .auth import auth
+    # from .auth import auth
+
+    # initialise SQLAlchemy with flask 
+    db.init_app(app)
 
     app.register_blueprint(views, url_prefix="/")
-    app.register_blueprint(auth, url_prefix="/")
+    # app.register_blueprint(auth, url_prefix="/")
 
+    from .models import Image
 
     return app
+
