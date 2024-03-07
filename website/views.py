cf20b430ea435cf611a7d4bdf65c077d653c462b
Y-Ibrahim <yibro.i2001@gmail.com>
2024-03-07 17:51:55 +0000

    Render Image table on gallery page

--- website/views.py
+++ website/views.py
@@ -1,8 +1,12 @@
 from flask import Blueprint, render_template
-
+from .models import Image
 views = Blueprint("views", __name__)
 
 @views.route("/")
-@views.route("/home")
 def home():
-    return render_template("home.html")
+    return render_template('home.html')
+
+@views.route('/gallery', methods=['GET', 'POST'])
+def gallery():
+    imageCards = Image.query.all()
+    return render_template('gallery.html', imageCards=imageCards)
