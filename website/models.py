cd647867875f0b6163eaf8ac6a89ca680a509444
Y-Ibrahim <yibro.i2001@gmail.com>
2024-03-20 11:49:46 +0000

    Updated table name to 'image'

--- website/models.py
+++ website/models.py
@@ -2,9 +2,11 @@ from . import db
 from sqlalchemy.sql import func
 
 
+
 class Image(db.Model):
+        __tablename__ = 'image'
         id = db.Column(db.Integer, primary_key=True)
         image_type = db.Column(db.String(100), nullable=False)
-        image_url = db.Column(db.String(100), nullable=False)
+        image_url = db.Column(db.String(2000), nullable=False)
         image_title = db.Column(db.String(100), nullable=False)
         image_text = db.Column(db.String(100), nullable=False)
